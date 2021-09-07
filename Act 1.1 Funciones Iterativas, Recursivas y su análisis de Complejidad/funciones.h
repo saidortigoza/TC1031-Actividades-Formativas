e8dc90323f50e425ed46148d9671c55e9164e154
Said Ortigoza <73250332+saidortigoza@users.noreply.github.com>
2021-09-07 16:32:13 -0500

    Add files via upload

---
+++ Act 1.1 Funciones Iterativas, Recursivas y su análisis de Complejidad/funciones.h
@@ -0,0 +1,35 @@
+//
+// Said Ortigoza
+// A01707430
+//
+
+#ifndef FUNCIONES_H
+#define FUNCIONES_H
+
+class Funciones {
+public:
+    Funciones(){};
+    
+    int sumaIterativa(int);
+    int sumaRecursiva(int);
+    int sumaDirecta(int);
+};
+
+int Funciones::sumaIterativa(int n){
+    int sum = 0, i;
+    for(i = 1; i <= n; i++){
+        sum += i;
+    }
+    return sum;
+}
+  
+int Funciones::sumaRecursiva(int n){
+    if(n == 0) return 0;
+    return n + sumaRecursiva(n - 1);
+}
+
+int Funciones::sumaDirecta(int n){
+    return (n * (n + 1) / 2);
+}
+
+#endif
